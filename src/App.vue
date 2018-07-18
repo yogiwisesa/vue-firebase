c7fb5afc23574dffea07559a7d6828a6b742bc43
yogiwisesa <yogiwisesa@gmail.com>
2018-07-18 22:22:34 +0700

    hello world!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div id="auth">
+      <a @click="register">Register</a>
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="(todo, idx) in todos" :key="idx">
+          <h3>{{ todo.todo }}</h3>
+          <p>{{ todo.color }}</p>
+          <p>{{ idx }}</p>
+          <button @click="deleteTodo(todo.id)">
+            Delete
+          </button>
+        </li>
+      </ul>
+      <input type="text" v-model="todo"> <br>
+      <input type="text" v-model="color">
+      <a @click="addTodo">Submit</a>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { db, auth } from "./main";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      email: "yogi2@gmail.com",
+      password: "1234567890",
+      todo: "",
+      color: "",
+      todos: [{ todo: "xx", color: "red" }]
+    };
+  },
+  firestore() {
+    return {
+      todos: db.collection("todos")
+    };
+  },
+  methods: {
+    register() {
+      console.log("register");
+      auth
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          console.log("Success");
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    },
+    addTodo() {
+      db
+        .collection("todos")
+        .add({
+          color: this.color,
+          todo: this.todo
+        })
+        .then(() => {
+          console.log("Success");
+        });
+    },
+    deleteTodo(id) {
+      db
+        .collection("todos")
+        .doc(id)
+        .delete();
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
+  /* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+Vue.use(VueFire)
+firebase.initializeApp({
+  apiKey: "AIzaSyAV-yRcR52f6Glc1RwBJhCwIobOz6vU8Ts",
+  authDomain: "konten-915a7.firebaseapp.com",
+  databaseURL: "https://konten-915a7.firebaseio.com",
+  projectId: "konten-915a7",
+  storageBucket: "konten-915a7.appspot.com",
+  messagingSenderId: "928889593190"
+})
+
+const firestore = firebase.firestore();
+const setting = {
+  timestampsInSnapshots: true
+}
+
+firestore.settings(setting);
+export const db = firestore;
+export const auth = firebase.auth()
+
 
 Vue.config.productionTip = false
 
